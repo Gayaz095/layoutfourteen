84d579c615290159ba74647ee9472d7473ebcafd
Gayaz095 <md.gayazahmed95@gmail.com>
2025-12-07 20:40:19 +0530

    updated path again

--- src/services/orderStatus.js
+++ src/services/orderStatus.js
@@ -1,7 +1,5 @@
-// const API_BASE_URL =
-//   import.meta.env.VITE_APP_BASE_URL || "http://localhost:3001/api";
-
-const API_BASE_URL = "https://layoutfourteenbackend.onrender.com/api";
+const API_BASE_URL =
+  import.meta.env.VITE_APP_BASE_URL || "http://localhost:3001/api";
 
 export const orderAPI = {
   getOrders: async () => {
