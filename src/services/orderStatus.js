3e6b51e04362dfb0860702d525b169e4d34c294b
Gayaz095 <md.gayazahmed95@gmail.com>
2025-12-07 20:05:01 +0530

    updated orderStatus

--- src/services/orderStatus.js
+++ src/services/orderStatus.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:3001/api";
+const API_BASE_URL =
+  process.env.REACT_APP_BASE_URL || "http://localhost:3001/api";
 
 export const orderAPI = {
   getOrders: async () => {
