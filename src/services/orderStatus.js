a60d1e7171299562e3075dbf358d1ff3c196859b
Gayaz095 <md.gayazahmed95@gmail.com>
2025-12-07 20:26:11 +0530

    testing orderStatus

--- src/services/orderStatus.js
+++ src/services/orderStatus.js
@@ -1,5 +1,7 @@
-const API_BASE_URL =
-  import.meta.env.VITE_APP_BASE_URL || "http://localhost:3001/api";
+// const API_BASE_URL =
+//   import.meta.env.VITE_APP_BASE_URL || "http://localhost:3001/api";
+
+const API_BASE_URL = "https://layoutfourteenbackend.onrender.com/api";
 
 export const orderAPI = {
   getOrders: async () => {
